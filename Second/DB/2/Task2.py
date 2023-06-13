129517bcb390d9565830b5c32afc965fa5b2e1f5
esunoalpha <8188431@mail.ru>
2023-06-13 13:28:13 +0300

    updated everything

--- Second/DB/2/Task2.py
+++ Second/DB/2/Task2.py
@@ -1,84 +1,83 @@
-"""
-Создайте модели базы данных работников it-компании
-Таблица Работники содержит следующие столбцы: id,имя,стаж, должности
-Таблица Должности содержит следующие столбцы: id, название, работники.
-Напишите функции вывода всех должностей запрашиваемого работника, всех работников по должности, всех работников определенной должности со стажем больше 5.
-"""
+from sqlalchemy import create_engine, Column, Integer, String, Table, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import relationship
-from sqlalchemy.orm import Session
+Base = declarative_base()
 
-sqlite_database = "sqlite:///it_company.db"
-engine = create_engine(sqlite_database)
+association_table = Table(
+    'association_table',
+    Base.metadata,
+    Column('worker_id', Integer, ForeignKey('workers.id')),
+    Column('position_id', Integer, ForeignKey('positions.id'))
+)
 
-
-class Base(DeclarativeBase):
-    pass
-
-
-class Workers(Base):
-    __tablename__ = "workers"
-    id = Column(Integer, primary_key=True, index=True)
+class Worker(Base):
+    __tablename__ = 'workers'
+    id = Column(Integer, primary_key=True)
     name = Column(String)
     experience = Column(Integer)
-    position_id = Column(Integer, ForeignKey("positions.id"))
-    position = relationship("Positions", back_populates="employees")
+    positions = relationship("Position", secondary=association_table, back_populates="workers")
 
-
-class Positions(Base):
-    __tablename__ = "positions"
-    id = Column(Integer, primary_key=True, index=True)
+class Position(Base):
+    __tablename__ = 'positions'
+    id = Column(Integer, primary_key=True)
     name = Column(String)
-    employees = relationship("Workers", back_populates="position")
+    workers = relationship("Worker", secondary=association_table, back_populates="positions")
 
+engine = create_engine('sqlite:///company.db')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
 
-Base.metadata.create_all(bind=engine)
+positions = [
+    Position(name="Сисадмин"),
+    Position(name="ИБшник")
+]
+session.add_all(positions)
+session.commit()
 
-with Session(autoflush=False, bind=engine) as db:
-    admin = Positions(name='ИБшник',
-                      employees=[Workers(name='Игнат', experience=3), Workers(name='Виталик', experience=5)])
-    developer = Positions(name='Сисадмин',
-                          employees=[Workers(name='Саша', experience=2), Workers(name='Катя', experience=7)])
-    db.add_all([admin, developer])
-    db.commit()
+workers = [
+    Worker(name="Катя", experience=3, positions=[positions[0]]),
+    Worker(name="Саша", experience=7, positions=[positions[1]])
+]
+session.add_all(workers)
+session.commit()
 
-
-def show_positions(worker_name):
-    with Session(bind=engine) as db:
-        worker = db.query(Workers).filter_by(name=worker_name).first()
-        if worker is None:
-            print(f'Работник {worker_name} не найден')
+def worker_positions(worker_name):
+    worker = session.query(Worker).filter_by(name=worker_name).first()
+    if worker:
+        positions = worker.positions
+        if positions:
+            print(f"Должности работника {worker_name}:")
+            for position in positions:
+                print(position.name)
         else:
-            positions = [worker.position.name]
-            print(f'Должности работника {worker_name}: {positions}')
+            print("У работника нет должностей.")
+    else:
+        print("Работник не найден.")
 
-
-def show_employees_by_position(position_name):
-    with Session(bind=engine) as db:
-        position = db.query(Positions).filter_by(name=position_name).first()
-        if position is None:
-            print(f'Должность {position_name} не найдена')
+def worker_by_position(position_name):
+    position = session.query(Position).filter_by(name=position_name).first()
+    if position:
+        workers = position.workers
+        if workers:
+            print(f"Работники с должностью {position_name}:")
+            for worker in workers:
+                print(worker.name)
         else:
-            employees = [e.name for e in position.employees]
-            print(f'Работники на должности {position_name}: {employees}')
+            print("Нет таких работников.")
+    else:
+        print("Нет такой должности.")
 
+def workerby_position_and_experience(position_name):
+    workers = session.query(Worker).join(association_table).join(Position).filter(Position.name == position_name, Worker.experience > 5).all()
+    if workers:
+        print(f"Работники на должности {position_name} со стажем больше 5:")
+        for worker in workers:
+            print(f"Имя: {worker.name}, Стаж: {worker.experience}")
+    else:
+        print(f"На должности нет работников со стажем больше 5.")
 
-def show_employees_by_position_and_experience(position_name):
-    with Session(bind=engine) as db:
-        employees = db.query(Workers).join(Positions).filter(
-            Positions.name == position_name,
-            Workers.experience > 5
-        ).all()
-        if not employees:
-            print(f'Работники с должностью {position_name} и стажем больше 5 лет не найдены')
-        else:
-            print(f'Работники на должности {position_name} со стажем более 5 лет:')
-            for employee in employees:
-                print(f'- {employee.name}')
-
-
-show_positions('Катя')
-show_employees_by_position('ИБшник')
-show_employees_by_position_and_experience('Сисадмин')
+worker_positions('Катя')
+worker_by_position('Сисадмин')
+workerby_position_and_experience('ИБшник')
